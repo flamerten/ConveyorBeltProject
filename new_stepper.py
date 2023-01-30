1ad077f8c2b05e76535538a293328fa556c43201
flamerten <flamerten@gmail.com>
2023-01-30 15:26:08 +0800

    update pins for the curtain

--- new_stepper.py
+++ new_stepper.py
@@ -8,10 +8,12 @@ ROL_STEP   = 20
 ROL_EN     = 21
 ROL_DELAY_TIME = 5000 * 0.0000001
 
-CUR_DIR    = 16
-CUR_STEP   = 20
-CUR_EN     = 21
+CUR_DIR    = 17
+CUR_STEP   = 27
+CUR_EN     = 22
 CUR_DELAY_TIME = 2500 * 0.0000001 #made 2x speed of roller?
+
+CUR_STEPS = 2000 #number of microsteps that the 
 
 ROCKER = 26
 BEAM   = 1
@@ -31,9 +33,10 @@ GPIO.setup(BEAM,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 
 while True:
     try:
-        if(GPIO.input(BEAM) == GPIO.LOW): #beam broken and not high
+        #check if beam has been broken AND rocker switch is turned on 
+        if( (GPIO.input(BEAM) == GPIO.LOW) and (GPIO.input(ROCKER) == GPIO.HIGH) ):
             print("Beam hit")
-            for i in range(2000):
+            for i in range(CUR_STEPS):
                 CurtainStepper.MoveMotor()
         else:
             if(GPIO.input(ROCKER) == GPIO.HIGH):
